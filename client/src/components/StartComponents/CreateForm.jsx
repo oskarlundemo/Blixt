c80c791ba172eea2161f265d77fd389f6a2322c1
oskarlundemo <bjornoskarstromberg@gmail.com>
2025-06-04 21:44:28 +0200

    Supabase auth
    Test commit

--- client/src/components/StartComponents/CreateForm.jsx
+++ client/src/components/StartComponents/CreateForm.jsx
@@ -78,6 +78,7 @@ export const CreateForm = ({setShowLogin}) => {
                 password,
             });
 
+
             if (signInError) {
                 setErrors([signInError.message]);
                 return;
